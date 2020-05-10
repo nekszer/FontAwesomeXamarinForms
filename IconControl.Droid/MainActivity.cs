4776f9fa973b40d6a6812fb0292683380471eaab
Daniel Antonio Lopez Espinoza <NEKSZER@hotmail.com>
2020-05-10 02:55:46 -0500

    'images'

--- IconControl.Droid/MainActivity.cs
+++ IconControl.Droid/MainActivity.cs
@@ -8,7 +8,7 @@ using Acr.UserDialogs;
 
 namespace IconControl.Droid
 {
-    [Activity(Label = "LightFormsTemplate", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "IconControl", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- IconControl.iOS/AppDelegate.cs
+++ IconControl.iOS/AppDelegate.cs
@@ -1,34 +1,21 @@
 ﻿using FFImageLoading.Forms.Platform;
 using Foundation;
-using LightForms;
-using IconControl.iOS.Services;
-using IconControl.Services;
 using Rg.Plugins.Popup;
 using UIKit;
 
 namespace IconControl.iOS
 {
     [Register("AppDelegate")]
-    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate, IPlatformInitializer
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
-
-        public LocalNotificationImplementation LocalNotification { get; private set; }
 
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             Popup.Init();
             CachedImageRenderer.Init();
             global::Xamarin.Forms.Forms.Init();
-            LoadApplication(new IconControl.App());
-            LocalNotification?.OnLaunching(options);
+            LoadApplication(new App());
             return base.FinishedLaunching(app, options);
-        }
-
-        public async void RegisterTypes(ICrossContainer container)
-        {
-            container.Register<ILocalNotification, LocalNotificationImplementation>(FetchTarget.Singleton);
-            LocalNotification = container.Create<ILocalNotification>() as LocalNotificationImplementation;
-            await LocalNotification.Init();
         }
     }
 }
